ad779e93ea5e17879ce331c5fdef51a53c3b29ae
Regolo alesandro <regoloalesandrogmail.com>
2019-10-09 20:10:56 -0300

    cambio en los test

--- Tp_test.py
+++ Tp_test.py
@@ -4,41 +4,27 @@ from pytest import *
 Lista_Llena = [1,2,3,4]
 
 
-def test_Length():
-  assert Length(Lista_Llena,1) == True
-  assert Length(Lista_Llena,5) == False
+def test_MayorA():
+  assert MayorA(Lista_Llena,1) == True
+  assert MayorA(Lista_Llena,5) == False
 
-def test_AgregaPersona():
-  Entrada1 = open("test1-1.txt","r")
-  Entrada2 = open("test1-2.txt","r")
+
+
+def test_Agregar():
+  Entrada1 = open("test1-1.txt", "r")
+  Entrada2 = open("test1-2.txt", "r")
   Lineas1 = Entrada1.readlines()
   Lineas2 = Entrada2.readlines()
-  persona1 = eval(Lineas1[0]
-  persona2 = eval(Lineas1[1]
-  persona3 = eval(Lineas1[2]
-  resultado1 = eval(Lineas2[0])
-  resultado2 = eval(Lineas2[1])
-  resultado3 = eval(Lineas2[2])
-  assert AgregaPersona(persona1,{persona1[2] : []}) == resultado1
-  assert AgregaPersona(persona2,{persona2[2] : []}) == resultado2
-  assert AgregaPersona(persona3,{persona3[2] : []}) == resultado3
-
-def test_Filtrado():
-  Entrada1 = open("test2-1.txt", "r")
-  Entrada2 = open("test2-2.txt", "r")
-  Lineas1 = Entrada1.readlines()
-  Lineas2 = Entrada2.readlines()
-  persona1 = eval(Lineas1[0]
+  persona1 = eval(Lineas1[0])
   candidatos1 = eval(Lineas1[1])
-  persona2 = eval(Lineas1[2]
-  persona3 = eval(Lineas1[3]
+  persona2 = eval(Lineas1[2])
+  persona3 = eval(Lineas1[3])
   resultado1 = eval(Lineas2[0])
   resultado2 = eval(Lineas2[1])
-  resultado3 = eval(Lineas2[2])
-  assert Filtrado(persona1,candidatos1) = resultado1  
-  assert Filtrado(pesona2,{}) =  resultado2
+  assert Agregar(persona1,candidatos1) == resultado1  
+  assert Agregar(persona2,{}) ==  resultado2
 
-def test_MatchingHomosexuals():
+def test_MatchingFunction():
   Salida = open("salida-test", "w+")
   Entrada1 = open("test3-1.txt", "r")
   Entrada2 = open("test3-2.txt", "r")
